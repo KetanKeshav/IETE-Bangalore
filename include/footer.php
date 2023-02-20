f93b05ed1f2f4ac8441d6b0c8d515b3b0edf0bb7
anzar00 <peerzada.anzar@gmail.com>
2023-02-21 00:35:38 +0530

    footer

--- include/footer.php
+++ include/footer.php
@@ -4,9 +4,8 @@
         <div class="row g-5">
             <div class="col-md-6 col-lg-3">
                 <h5 class="text-white mb-4">Get In Touch</h5>
-                <p><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-                <p><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                <p><i class="fa fa-envelope me-3"></i>info@example.com</p>
+                
+                <img src="img/f1.png" width="175px" height="150px"/>
                 <div class="d-flex pt-2">
                     <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
                     <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
@@ -16,43 +15,30 @@
                 </div>
             </div>
             <div class="col-md-6 col-lg-3">
-                <h5 class="text-white mb-4">Popular Link</h5>
+                <h5 class="text-white mb-4">Useful Links</h5>
                 <a class="btn btn-link" href="">About Us</a>
                 <a class="btn btn-link" href="">Contact Us</a>
                 <a class="btn btn-link" href="">Privacy Policy</a>
                 <a class="btn btn-link" href="">Terms & Condition</a>
                 <a class="btn btn-link" href="">Career</a>
             </div>
+
             <div class="col-md-6 col-lg-3">
-                <h5 class="text-white mb-4">Project Gallery</h5>
-                <div class="row g-2">
-                    <div class="col-4">
-                        <img class="img-fluid" src="img/portfolio-1.jpg" alt="Image">
-                    </div>
-                    <div class="col-4">
-                        <img class="img-fluid" src="img/portfolio-2.jpg" alt="Image">
-                    </div>
-                    <div class="col-4">
-                        <img class="img-fluid" src="img/portfolio-3.jpg" alt="Image">
-                    </div>
-                    <div class="col-4">
-                        <img class="img-fluid" src="img/portfolio-4.jpg" alt="Image">
-                    </div>
-                    <div class="col-4">
-                        <img class="img-fluid" src="img/portfolio-5.jpg" alt="Image">
-                    </div>
-                    <div class="col-4">
-                        <img class="img-fluid" src="img/portfolio-6.jpg" alt="Image">
-                    </div>
-                </div>
+                <h5 class="text-white mb-4">Quick Links</h5>
+                <a class="btn btn-link" href="">About Us</a>
+                <a class="btn btn-link" href="">Contact Us</a>
+                <a class="btn btn-link" href="">Privacy Policy</a>
+                <a class="btn btn-link" href="">Terms & Condition</a>
+                <a class="btn btn-link" href="">Career</a>
             </div>
+            
             <div class="col-md-6 col-lg-3">
-                <h5 class="text-white mb-4">Newsletter</h5>
-                <p>Lorem ipsum dolor sit amet elit. Phasellus nec pretium mi. Curabitur facilisis ornare velit non vulpu</p>
-                <div class="position-relative w-100 mt-3">
-                    <input class="form-control border-0 rounded-pill w-100 ps-4 pe-5" type="text" placeholder="Your Email" style="height: 48px;">
-                    <button type="button" class="btn shadow-none position-absolute top-0 end-0 mt-1 me-2"><i class="fa fa-paper-plane text-primary fs-4"></i></button>
-                </div>
+                <h5 class="text-white mb-4">Contact Us</h5>
+                <p><i class="fa fa-map-marker-alt me-3"></i>IETE Bangalore Centre Bellary Road, Ganganagar Extension, Bangalore - 560 032 India</p>
+                <p><i class="fa fa-mobile-alt me-3"></i>+91 99017 43330</p>
+                <p><i class="fa fa-phone-alt me-3"></i>080 - 23331133</p>
+                <p><i class="fa fa-fax me-3"></i>080 - 23337231</p>
+                <p><i class="fa fa-envelope me-3"></i>bangalore@iete.org</p>
             </div>
         </div>
     </div>
@@ -60,11 +46,7 @@
         <div class="copyright">
             <div class="row">
                 <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                    &copy; <a class="border-bottom" href="#">Your Site Name</a>, All Right Reserved.
-
-                    <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                    Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
-                    <br>Distributed By: <a class="border-bottom" href="https://themewagon.com" target="_blank">ThemeWagon</a>
+                    &copy; <a class="border-bottom" href="#">IETE Bangalore</a>
                 </div>
                 <div class="col-md-6 text-center text-md-end">
                     <div class="footer-menu">
