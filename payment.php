2820fcd2bc2a55de433864ff459db2ecf33f3c38
anzar000 <peerzada.anzar@gmail.com>
2023-03-17 10:12:31 +0530

    Payments

--- payment.php
+++ payment.php
@@ -59,7 +59,7 @@
                     <h2 class="mt-2">Payment Details</h2>
                 </div>
                 <div class="row g-5">
-                    <div class="col-lg-12 wow fadeInUp" data-wow-delay="0.1s">
+                    <!-- <div class="col-lg-12 wow fadeInUp" data-wow-delay="0.1s">
                         <p class="mb-4 text-justify">
                             All payments are to be made by online/Demand Draft drawn in
                             favour of <b>"IICI Bangalore"</b> Payable at Bangalore with
@@ -70,9 +70,29 @@
                             Account Type : <b>Current Account</b><br>
                             Bank Name : <b>State Bank of India (SBI), Ganganahalli Bangalore</b><br>
                             IFSCCode : <b>SBIN0040276</b>
-
                         </p>
-
+                    </div> -->
+                    <div class="col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <h2 class="text-center">
+                            Google form link for IETE Bangalore Payment<br>
+                            <a href="https://forms.gle/YV7YC5zpyng8nJ7P9">IETE Bangalore</a>
+                        </h2>
+                        <div class="img-fluid">
+                            <center>
+                                <img src="img/payments/1.webp" alt="payment" class="img-fluid" width="70%">
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <h2 class="text-center">
+                            Google form link for IICI 2023<br>
+                            <a href="https://forms.gle/SJAzUExDdmTaq9wW7">IICI 2023</a>
+                        </h2>
+                        <div class="img-fluid">
+                            <center>
+                                <img src="img/payments/2.webp" alt="payment" class="img-fluid" width="70%">
+                            </center>
+                        </div>
                     </div>
                 </div>
             </div>
