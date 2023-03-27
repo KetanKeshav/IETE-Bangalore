1b205a580bb388d1b4e87c4dbf8a41fa8435ef07
anzar000 <peerzada.anzar@gmail.com>
2023-03-27 16:30:59 +0530

    git add .

--- include/footer.php
+++ include/footer.php
@@ -1,17 +1,17 @@
 <!-- Footer Start -->
-<div class="container-fluid text-light footer mt-5 pt-5 wow fadeIn" style="background-color: #2124b1" data-wow-delay="0.1s">
+<div class="container-fluid text-light footer pt-5 wow fadeIn" style="background-color: #2124b1;" data-wow-delay="0.1s">
     <div class="container py-5 px-lg-5">
         <div class="row g-5">
             <div class="col-md-6 col-lg-3">
                 <h5 class="text-white mb-4">Get In Touch</h5>
                 
                 <img src="img/f1.png" width="175px" height="150px"/>
-                <div class="d-flex pt-2">
-                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
-                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
-                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-instagram"></i></a>
-                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
+                <div class="d-flex pt-2 text-white">
+                    <a class="btn btn-outline-light btn-social" href=""><i class="text-white fab fa-twitter"></i></a>
+                    <a class="btn btn-outline-light btn-social" href=""><i class="text-white fab fa-facebook-f"></i></a>
+                    <a class="btn btn-outline-light btn-social" href=""><i class="text-white fab fa-youtube"></i></a>
+                    <a class="btn btn-outline-light btn-social" href=""><i class="text-white fab fa-instagram"></i></a>
+                    <a class="btn btn-outline-light btn-social" href=""><i class="text-white fab fa-linkedin-in"></i></a>
                 </div>
             </div>
             <div class="col-md-6 col-lg-3">
@@ -32,18 +32,18 @@
             
             <div class="col-md-6 col-lg-3">
                 <h5 class="text-white mb-4">Contact Us</h5>
-                <p><i class="fa fa-map-marker-alt me-3"></i>IETE Bangalore Centre Bellary Road, Ganganagar Extension, Bangalore - 560 032 India</p>
-                <p><i class="fa fa-mobile-alt me-3"></i>+91 99017 43330</p>
-                <p><i class="fa fa-phone-alt me-3"></i>080 - 23331133</p>
-                <p><i class="fa fa-fax me-3"></i>080 - 23337231</p>
-                <p><i class="fa fa-envelope me-3"></i>bangalore@iete.org</p>
+                <p class="text-white"><i class="text-white fa fa-map-marker-alt me-3"></i>IETE Bangalore Centre Bellary Road, Ganganagar Extension, Bangalore - 560 032 India</p>
+                <p class="text-white"><i class="text-white fa fa-mobile-alt me-3"></i>+91 99017 43330</p>
+                <p class="text-white"><i class="text-white fa fa-phone-alt me-3"></i>080 - 23331133</p>
+                <p class="text-white"><i class="text-white fa fa-fax me-3"></i>080 - 23337231</p>
+                <p class="text-white"><i class="text-white fa fa-envelope me-3"></i>bangalore@iete.org</p>
             </div>
         </div>
     </div>
     <div class="container px-lg-5">
         <div class="copyright">
             <div class="row">
-                <div class="col-md-12 text-center text-md-start mb-3 mb-md-0">
+                <div class="col-md-12 text-center text-white text-md-start mb-3 mb-md-0">
                     &copy; <a class="border-bottom" href="index.php">IETE Bangalore</a>
                 </div>
             </div>
