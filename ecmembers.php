5a331a59af7c305849f16c0748df26b3833241fc
Ketan Keshav <ketan.keshav7@gmail.com>
2023-04-07 14:26:47 +0530

    EC mem update

--- ecmembers.php
+++ ecmembers.php
@@ -62,12 +62,6 @@
                     <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                         <div class="team-item">
                             <div class="d-flex">
-                                <div class="flex-shrink-0 d-flex flex-column align-items-center mt-4 pt-5" style="width: 75px;">
-                                    <a class="btn btn-square text-primary bg-white my-1" href=""><i class="fab fa-facebook-f"></i></a>
-                                    <a class="btn btn-square text-primary bg-white my-1" href=""><i class="fab fa-twitter"></i></a>
-                                    <a class="btn btn-square text-primary bg-white my-1" href=""><i class="fab fa-instagram"></i></a>
-                                    <a class="btn btn-square text-primary bg-white my-1" href=""><i class="fab fa-linkedin-in"></i></a>
-                                </div>
                                 <img class="img-fluid rounded w-100" src="img/team/1.webp" alt="">
                             </div>
                             <div class="px-4 py-3">
