16471eda8388a4eb14144839b3133fcddbc05727
Ketan Keshav <ketan.keshav7@gmail.com>
2023-04-12 23:36:30 +0530

    IICI update

--- include/navbar.php
+++ include/navbar.php
@@ -32,6 +32,7 @@
                     <a href="iici-cfs.php" class="dropdown-item  <?php if($page == 'iici-cfs') echo 'active' ?>">Call For Sponsors</a>
                     <a href="iici-registration.php" class="dropdown-item <?php if($page == 'iici-registration') echo 'active' ?>">Registration</a>
                     <a href="iici-dates.php" class="dropdown-item <?php if($page == 'iici-dates') echo 'active' ?>">Important Dates</a>
+                    <a href="https://iici.ietebangalore.org/" target="_blank" class="dropdown-item">Full Website</a>
                 </div>
             </div>
             <!-- <a href="iici.php" class="nav-item nav-link <?php if($page == 'iici') echo 'active' ?>">IICI 2023</a> -->
